aa7d53d286e1fe033ce32dff0f0e72ec9e8cda54
Jason Devore <sqyttles@gmail.com>
2016-08-28 18:28:40 -0500

    async getcurrentuser

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,20 +1,34 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
 using AJI;
 using AJI.Data;
 using AJI.Models;
+using AJI.Models.AccountViewModels;
+using AJI.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace AJI.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private ApplicationDbContext _context;
 
-        public AdminController(ApplicationDbContext context)
+        public AdminController(UserManager<ApplicationUser> userManager,
+                                SignInManager<ApplicationUser> signInManager,
+                                ApplicationDbContext context)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
             _context = context;
         }
         public IActionResult Index()
@@ -41,9 +55,10 @@ namespace AJI.Controllers
 
         [HttpPostAttribute]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Post post) //[FromBodyAttribute]
+        public async Task<IActionResult> Create(Post post) //[FromBodyAttribute]
         {
             // post.Author = _context.Users.Where(u => u.)
+            post.Author = await GetCurrentUserAsync();
             post.ModifiedOn = DateTime.Now;
             post.CreatedOn = DateTime.Now;
             if (ModelState.IsValid)
@@ -65,6 +80,11 @@ namespace AJI.Controllers
         public IActionResult Delete(Post post)
         {
             return View();
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return _userManager.GetUserAsync(HttpContext.User);
         }
     }
 }
