66464747002b5d6abbb2e08566264b6b26ec954d
Jason Devore <sqyttles@gmail.com>
2016-08-28 23:14:25 -0500

    Remove unnecessary usings form controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.Extensions.Logging;
-using AJI;
 using AJI.Data;
-using AJI.Models;
-using AJI.Models.AccountViewModels;
-using AJI.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace AJI.Controllers
